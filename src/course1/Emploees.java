23635671c342d6c526cf134b8fc73374a9d2d72c
ermakovolegyurievch <ermakovolegyurievch@gmail.com>
2023-04-10 13:18:04 +0400

    ToHashMap.v1

--- src/course1/Emploees.java
+++ src/course1/Emploees.java
@@ -1,21 +1,15 @@
 package course1;
 
-class Employees {
-    public static Employee[] abc = new Employee[10];
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
+class Employees {
+
+    private static final Map<String, Employee> employeesBook = new HashMap<>();
     public static void main(String[] args) {
-        abc[0] = new Employee("Иванов", "Иван", "Иваныч", 1, 30.000);
-        abc[1] = new Employee("Петров", "Петр", "Петрович", 2, 40.000);
-        abc[2] = new Employee("Сергеев", "Сергей", "Сергеевич", 3, 10.000);
-        abc[3] = new Employee("Андреев", "Андрей", "Андреевич", 4, 20.000);
-        abc[4] = new Employee("Алексеев", "Алексей", "Алексеевич", 2, 50.000);
-        abc[5] = new Employee("Дмитриев", "Дмитрий", "Дмитриевич", 3, 30.000);
-        abc[6] = new Employee("Владимиров", "Владимир", "Владимирович", 5, 70.000);
-        abc[7] = new Employee("Сидоров", "Сидор", "Сидорович", 3, 20.000);
-        abc[8] = new Employee("Максимов", "Максим", "Максимович", 4, 40.000);
-        abc[9] = new Employee("Ильин", "Илья", "Ильич", 1, 50.000);
         allEmployees();
-        allFullNames();
+        allFullNames(employeesBook);
         sumSalary();
         minSalaryEmploy();
         maxSalaryEmploy();
@@ -23,56 +17,71 @@ class Employees {
 
     }
 
-    public static void allEmployees() {
-        System.out.println("Полные данные по всем сотрудникам:");
-        for (Employee i : abc) {
-            System.out.println(i);
-        }
+    private static String getKey(Employee employee) {
+        return employee.getSurName() + " " + employee.getFirstName() + " " + employee.getSecondName();
     }
 
-    public static void allFullNames() {
-        System.out.println("Список сотрудников:");
-        for (Employee obj : abc) {
-            System.out.print(obj.getSurName() + " ");
-            System.out.print(obj.getFirstName() + " ");
-            System.out.print(obj.getSecondName() + " ");
-            System.out.println(" ");
-        }
+    public static void add(Employee employee) {
+        employeesBook.put(getKey(employee), employee);
     }
 
-    public static void sumSalary() {
-        double sum = 0;
-        for (Employee obj : abc) {
-            sum += obj.getSalary();
-        }
-        System.out.println("Сумма всех зарплат - " + sum);
+    public static void remove(Employee employee) {
+        employeesBook.remove(getKey(employee));
     }
 
-    public static void minSalaryEmploy() {
-        double minSalary = Integer.MAX_VALUE;
-        for (Employee obj : abc) {
-            if (minSalary > obj.getSalary()) {
-                minSalary = obj.getSalary();
+    public static boolean find(Employee employee) {
+        return (employeesBook.containsKey(getKey(employee)));
+        }
+
+        public static void allEmployees () {
+            System.out.println("Полные данные по всем сотрудникам:");
+            for (Employee emp : employeesBook.values()) {
+                System.out.println(emp);
             }
         }
-        System.out.println("Минимальная зарплата - " + minSalary);
-    }
 
-    public static void maxSalaryEmploy() {
-        double maxSalary = Integer.MIN_VALUE;
-        for (Employee obj : abc) {
-            if (maxSalary < obj.getSalary()) {
-                maxSalary = obj.getSalary();
+
+        public static void allFullNames (Map<String,Employee>map) {
+            System.out.println("Список сотрудников:");
+            for (Employee emp : map.values()) {
+                System.out.println(emp.getSurName() + " " + emp.getFirstName() + " " + emp.getSecondName());
             }
         }
-        System.out.println("Максимальная зарплата - " + maxSalary);
-    }
 
-    public static void middleSalaryAllEmployees() {
-        double sum = 0;
-        for (Employee obj : abc) {
-            sum += obj.getSalary();
+
+        public static void sumSalary () {
+            double sum = 0;
+            for (Employee emp : employeesBook.values()) {
+                sum += emp.getSalary();
+            }
+            System.out.println("Сумма всех зарплат - " + sum);
         }
-        System.out.println("Средняя зарплата - " + sum / abc.length);
+
+        public static void minSalaryEmploy () {
+            double minSalary = Integer.MAX_VALUE;
+            for (Employee emp : employeesBook.values()) {
+                if (minSalary > emp.getSalary()) {
+                    minSalary = emp.getSalary();
+                }
+            }
+            System.out.println("Минимальная зарплата - " + minSalary);
+        }
+
+        public static void maxSalaryEmploy () {
+            double maxSalary = Integer.MIN_VALUE;
+            for (Employee emp : employeesBook.values()) {
+                if (maxSalary < emp.getSalary()) {
+                    maxSalary = emp.getSalary();
+                }
+            }
+            System.out.println("Максимальная зарплата - " + maxSalary);
+        }
+
+        public static void middleSalaryAllEmployees () {
+            double sum = 0;
+            for (Employee emp : employeesBook.values()) {
+                sum += emp.getSalary();
+            }
+            System.out.println("Средняя зарплата - " + sum / employeesBook.size());
+        }
     }
-}
